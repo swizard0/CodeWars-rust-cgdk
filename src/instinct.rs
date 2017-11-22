e0ae621ffacd66b35955a5de70482fee7963b9d5
Alexey Voznyuk <me@swizard.info>
2017-11-22 22:15:59 +0300

    some performance and design related refactoring

--- src/instinct.rs
+++ src/instinct.rs
@@ -28,14 +28,15 @@ pub fn run<'a, R>(mut form: FormationRef<'a>, world: &World, tactic: &mut Tactic
 
     enum Reaction {
         KeepOn,
-        ComeUpWithSomething,
+        GoCurious,
+        CloseRanks,
         RunAway,
     }
 
-    let reaction = match (form.current_plan(), trigger) {
+    let mut reaction = match (form.current_plan(), trigger) {
         // nothing annoying around and we don't have a plan: let's do something
         (&mut None, Trigger::None) =>
-            Reaction::ComeUpWithSomething,
+            Reaction::GoCurious,
         // nothing annoying around, keep following the plan
         (&mut Some(..), Trigger::None) =>
             Reaction::KeepOn,
@@ -50,78 +51,100 @@ pub fn run<'a, R>(mut form: FormationRef<'a>, world: &World, tactic: &mut Tactic
             Reaction::RunAway,
         // we are not moving and also don't have a plan: let's do something
         (&mut None, Trigger::Idle) =>
-            Reaction::ComeUpWithSomething,
-        // we are currently scouting and eventually stopped: let's scout another way
-        (&mut Some(Plan { desire: Desire::ScoutTo { .. }, .. }), Trigger::Idle) =>
-            Reaction::ComeUpWithSomething,
-        // we are currently making formation more compact and eventually stopped: let's do something
+            Reaction::GoCurious,
+        // we are currently making formation more compact and eventually stopped: let's continue with something useful
         (&mut Some(Plan { desire: Desire::Compact { .. }, .. }), Trigger::Idle) =>
-            Reaction::ComeUpWithSomething,
+            Reaction::GoCurious,
+        // we are currently scouting and eventually stopped: maybe we should make formation more compact
+        (&mut Some(Plan { desire: Desire::ScoutTo { .. }, .. }), Trigger::Idle) =>
+            Reaction::CloseRanks,
         // we are currently attacking and also not moving: keep attacking then
         (&mut Some(Plan { desire: Desire::Attack { .. }, .. }), Trigger::Idle) =>
-            Reaction::ComeUpWithSomething,
+            Reaction::CloseRanks,
         // we are currently escaping and eventually stopped: looks like we are safe, so go ahead do something
         (&mut Some(Plan { desire: Desire::Escape { .. }, ..}), Trigger::Idle) =>
-            Reaction::ComeUpWithSomething,
+            Reaction::CloseRanks,
 
         // it is supposed we cannot have this scenario
         (&mut Some(Plan { desire: Desire::FormationSplit { .. }, ..}), _) =>
             unreachable!(),
     };
 
+    // apply some post checks and maybe change reaction
+    loop {
+        match reaction {
+            // ensure that we really need to make formation more compact
+            Reaction::CloseRanks => if form.bounding_box().density < consts::COMPACT_DENSITY {
+                break;
+            } else {
+                reaction = Reaction::GoCurious;
+            },
+            _ =>
+                break,
+        }
+    }
+
     match reaction {
         Reaction::KeepOn =>
             (),
-        Reaction::ComeUpWithSomething =>
-            scout_etc(form, world, tactic, rng),
+        Reaction::GoCurious =>
+            scout(form, world, tactic, rng),
+        Reaction::CloseRanks =>
+            compact(form, world, tactic),
         Reaction::RunAway =>
             run_away(form, world, tactic, rng),
     }
 }
 
-fn scout_etc<'a, R>(mut form: FormationRef<'a>, world: &World, tactic: &mut Tactic, rng: &mut R) where R: Rng {
+fn scout<'a, R>(mut form: FormationRef<'a>, world: &World, tactic: &mut Tactic, rng: &mut R) where R: Rng {
     let x = rng.gen_range(0., world.width);
     let y = rng.gen_range(0., world.height);
-    let bbox = form.bounding_box().clone();
-    let fx = bbox.cx;
-    let fy = bbox.cy;
-    let do_compact = if let &mut Some(Plan { desire: Desire::Compact { .. }, .. }) = form.current_plan() {
-        false
-    } else if bbox.density < consts::COMPACT_DENSITY {
-        true
-    } else {
-        false
+    let (fx, fy) = {
+        let bbox = form.bounding_box();
+        (bbox.cx, bbox.cy)
     };
     tactic.plan(Plan {
         form_id: form.id,
         tick: world.tick_index,
-        desire: if do_compact {
-            Desire::Compact {
-                fx: (bbox.left + bbox.right) / 2.,
-                fy: (bbox.top + bbox.bottom) / 2.,
-                kind: form.kind().clone(),
-                density: bbox.density,
-            }
-        } else {
-            Desire::ScoutTo {
-                fx, fy, x, y,
-                kind: form.kind().clone(),
-                sq_dist: sq_dist(fx, fy, x, y),
-            }
+        desire: Desire::ScoutTo {
+            fx, fy, x, y,
+            kind: form.kind().clone(),
+            sq_dist: sq_dist(fx, fy, x, y),
+        },
+    });
+}
+
+fn compact<'a>(mut form: FormationRef<'a>, world: &World, tactic: &mut Tactic) {
+    let (fx, fy, density) = {
+        let bbox = form.bounding_box();
+        ((bbox.left + bbox.right) / 2.,
+         (bbox.top + bbox.bottom) / 2.,
+         bbox.density)
+    };
+    tactic.plan(Plan {
+        form_id: form.id,
+        tick: world.tick_index,
+        desire: Desire::Compact {
+            fx, fy, density,
+            kind: form.kind().clone(),
         },
     });
 }
 
 fn run_away<'a, R>(mut form: FormationRef<'a>, world: &World, tactic: &mut Tactic, rng: &mut R) where R: Rng {
-    let bbox = form.bounding_box().clone();
+    let (fx, fy) = {
+        let bbox = form.bounding_box();
+        (bbox.cx, bbox.cy)
+    };
+
     // try to detect right escape direction
     let (escape_coord, d_durability) = {
         let (dvts, count) = form.dvt_sums(world.tick_index);
         let coord = if dvts.d_x == 0. && dvts.d_y == 0. {
             None
         } else {
-            let x = bbox.cx - (dvts.d_x * consts::ESCAPE_BOUNCE_FACTOR / count as f64);
-            let y = bbox.cy - (dvts.d_y * consts::ESCAPE_BOUNCE_FACTOR / count as f64);
+            let x = fx - (dvts.d_x * consts::ESCAPE_BOUNCE_FACTOR / count as f64);
+            let y = fy - (dvts.d_y * consts::ESCAPE_BOUNCE_FACTOR / count as f64);
             if x > 0. && x < world.width && y > 0. && y < world.height {
                 Some((x, y))
             } else {
@@ -141,9 +164,7 @@ fn run_away<'a, R>(mut form: FormationRef<'a>, world: &World, tactic: &mut Tacti
         form_id: form.id,
         tick: world.tick_index,
         desire: Desire::Escape {
-            x, y,
-            fx: bbox.cx,
-            fy: bbox.cy,
+            x, y, fx, fy,
             danger_coeff: 0. - (d_durability as f64),
         },
     });
