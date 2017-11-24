14e01036ce669cc24f534358e6c5e68f0a0638d6
Alexey Voznyuk <me@swizard.info>
2017-11-24 20:11:34 +0300

    some kind of collisions predictor

--- src/rect.rs
+++ src/rect.rs
@@ -41,4 +41,55 @@ impl Rect {
     pub fn inside(&self, x: f64, y: f64) -> bool {
         x >= self.left && x <= self.right && y >= self.top && y <= self.bottom
     }
+
+    pub fn sq_radius(&self) -> f64 {
+        let wl = self.cx - self.left;
+        let wr = self.right - self.cx;
+        let w = wl.max(wr);
+        let ht = self.cy - self.top;
+        let hb = self.bottom - self.cy;
+        let h = ht.max(hb);
+        (w * w) + (h * h)
+    }
+
+    pub fn sq_dist_to_line(&self, from_x: f64, from_y: f64, to_x: f64, to_y: f64) -> f64 {
+        let upper = (to_x - from_x) * (self.cy - from_y) - (to_y - from_y) * (self.cx - from_x);
+        let upper_sq = upper * upper;
+        let lower_sq = (to_x - from_x) * (to_x - from_x) + (to_y - from_y) * (to_y - from_y);
+        upper_sq / lower_sq
+    }
+
+    pub fn predict_collision(&self, from_x: f64, from_y: f64, to_x: f64, to_y: f64) -> bool {
+        self.sq_dist_to_line(from_x, from_y, to_x, to_y) <= self.sq_radius()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Rect;
+
+    #[test]
+    fn sq_radius() {
+        let ra = Rect { left: 10., top: 10., right: 14.0, bottom: 13.0, cx: 12., cy: 11.5, ..Default::default() };
+        assert_eq!(ra.sq_radius(), 6.25);
+        let rb = Rect { left: 10., top: 10., right: 15.0, bottom: 14.0, cx: 11., cy: 13., ..Default::default() };
+        assert_eq!(rb.sq_radius(), 25.);
+    }
+
+    #[test]
+    fn sq_dist_to_line() {
+        let ra = Rect { left: 10., top: 10., right: 14.0, bottom: 14.0, cx: 12., cy: 12., ..Default::default() };
+        assert_eq!(ra.sq_dist_to_line(10.0, 10.0, 14.0, 10.0), 4.);
+        assert_eq!(ra.sq_dist_to_line(10.0, 16.0, 14.0, 16.0), 16.);
+        assert_eq!(ra.sq_dist_to_line(10.0, 10.0, 10.0, 14.0), 4.);
+        assert_eq!(ra.sq_dist_to_line(16.0, 10.0, 16.0, 14.0), 16.);
+        assert_eq!(ra.sq_dist_to_line(8.0, 12.0, 12.0, 8.0), 8.);
+    }
+
+    #[test]
+    fn predict_collision() {
+        let ra = Rect { left: 10., top: 10., right: 15.0, bottom: 14.0, cx: 11., cy: 13., ..Default::default() };
+        assert!(!ra.predict_collision(9., 7., 13., 7.0));
+        assert!(ra.predict_collision(9., 9., 13., 9.0));
+    }
 }
