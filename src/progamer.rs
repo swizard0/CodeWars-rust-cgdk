de4234afabf4ff2af9980397c4c78a53592d28d0
Alexey Voznyuk <me@swizard.info>
2017-11-25 01:55:24 +0300

    bugfix

--- src/progamer.rs
+++ src/progamer.rs
@@ -188,7 +188,7 @@ impl Progamer {
                 let form_id = form.id;
                 action.vehicle_type = form.kind().clone();
                 let bbox = form.bounding_box();
-                debug!("selecting unbound formation {} of {:?}", form_id, form.kind());
+                debug!("selecting unbound formation {} of {:?}", form_id, action.vehicle_type);
                 action.action = Some(ActionType::ClearAndSelect);
                 action.left = bbox.left;
                 action.top = bbox.top;
