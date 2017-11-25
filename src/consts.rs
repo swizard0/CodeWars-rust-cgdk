47cc73b4f08731bb470ccdecf17afafc7e8de74d
Alexey Voznyuk <me@swizard.info>
2017-11-25 19:14:21 +0300

    larger hunt range factor

--- src/consts.rs
+++ src/consts.rs
@@ -5,5 +5,5 @@ pub const ESCAPE_BOUNCE_FACTOR: f64 = 80.0;
 pub const SPLIT_MAX_FORMS: usize = 12;
 pub const HELPER_BY_ESCAPE_DIST_RATIO_SQ: f64 = 4.0 * 4.0;
 pub const SCOUT_RANGE_FACTOR: f64 = 2.0;
-pub const HUNT_RANGE_FACTOR: f64 = 4.0;
+pub const HUNT_RANGE_FACTOR: f64 = 3.0;
 pub const NUKE_ENEMY_CLOSENESS: f64 = 1.25;
